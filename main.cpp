309293f61e3ecbc24d07f9dcca9bafb8ef0f20c3
hemoi <happy97cjh@gmail.com>
2020-08-04 02:16:40 +0900

    Test finished

--- main.cpp
+++ main.cpp
@@ -2,8 +2,6 @@
 #include <pcap.h>
 #include "ethhdr.h"
 #include "arphdr.h"
-// #include "ip.h"
-#include "myAddr.h"
 #include <string>
 #include <sys/types.h>
 #include <sys/socket.h>
@@ -143,11 +141,8 @@ int main(int argc, char* argv[]) {
 	// ARP reply 수신
 	// 패킷을 받고
 	while (true) {
-		sleep(0);
-		struct pcap_pkthdr* header;
-		const u_char*		packet;
-		const EthHdr*		ethernet;
-		const ArpHdr*		arp;
+		pcap_pkthdr* header;
+		const u_char*	packet;
 
 		int res = pcap_next_ex(handle, &header, &packet);
         if (res == 0) continue;
@@ -156,17 +151,19 @@ int main(int argc, char* argv[]) {
             break;
         }
 
-		ethernet = (EthHdr*)(packet);
-		if(ethernet->type_ != EthHdr().Arp || ethernet->dmac_ != myMac){
-			continue;
+		EthHdr* ethernet = (EthHdr*)(packet);
+		
+		if(ethernet->type() != EthHdr::Arp){
+		 	continue;
 		};
 
-		arp = (ArpHdr*)(packet + sizeof(EthHdr));
-		if(arp->tip_ != myIp) {
+		ArpHdr* arp = (ArpHdr*)((uint8_t*)packet + 14);
+		if(arp->tip() != myIp || arp->op() != ArpHdr::Reply) {
 			continue;
 		}
 
-		Mac senderMac = arp->smac_;
+		senderMac = arp->smac();
+		
 		break;
 
 	}
@@ -176,25 +173,6 @@ int main(int argc, char* argv[]) {
 		fprintf(stderr, "pcap_sendpacket return %d error=%s\n", res, pcap_geterr(handle));
 	}
 
-/*
-	packet.eth_.dmac_ = Mac("00:00:00:00:00:00");
-	packet.eth_.smac_ = Mac("00:00:00:00:00:00");
-	packet.eth_.type_ = htons(EthHdr::Arp);
-
-	packet.arp_.hrd_ = htons(ArpHdr::ETHER);
-	packet.arp_.pro_ = htons(EthHdr::Ip4);
-	packet.arp_.hln_ = Mac::SIZE;
-	packet.arp_.pln_ = Ip::SIZE;
-	packet.arp_.op_ = htons(ArpHdr::Request);
-	packet.arp_.smac_ = Mac("00:00:00:00:00:00");
-	packet.arp_.sip_ = htonl(Ip("0.0.0.0"));
-	packet.arp_.tmac_ = Mac("00:00:00:00:00:00");
-	packet.arp_.tip_ = htonl(targetIp);
-	res = pcap_sendpacket(handle, reinterpret_cast<const u_char*>(&packet), sizeof(EthArpPacket));
-	if (res != 0) {
-		fprintf(stderr, "pcap_sendpacket return %d error=%s\n", res, pcap_geterr(handle));
-	}
-*/
 	printf("finish");
 	pcap_close(handle);
 }
